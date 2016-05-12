fd8d45c2c9ba5fd41e5a7b380f78c9a4f0700571
ojalvo@wisc.edu <ojalvo@wisc.edu>
2016-05-12 15:17:57 +0200

    adding stage2 branch

--- test/submitStage2MC.sh
+++ test/submitStage2MC.sh
@@ -3,20 +3,20 @@ voms-proxy-init --voms cms --valid 100:00
 cat testL1TStage2RatesEff.py > SUBStage2.py
 cat submitSecondaryFiles.py >> SUBStage2.py
 
-rm -r /data/ojalvo/ggHttMC-SUBStage2/
-mkdir /data/ojalvo/ggHttMC-SUBStage2/
-#submit dir = /data/ojalvo/ggHttMC-SUBStage2/submit
+rm -r /data/ojalvo/ggHttMC-2-SUBStage2/
+mkdir /data/ojalvo/ggHttMC-2-SUBStage2/
+#submit dir = /data/ojalvo/ggHttMC-2-SUBStage2/submit
 #make dag dir
-mkdir -p /data/ojalvo/ggHttMC-SUBStage2/dags
-mkdir -p /data/ojalvo/ggHttMC-SUBStage2/dags/daginputs
-## outputdir = srm://cmssrm.hep.wisc.edu:8443/srm/v2c2/server?SFN=/hdfs/store/user/ojalvo/ggHttMC-SUBStage2/
+mkdir -p /data/ojalvo/ggHttMC-2-SUBStage2/dags
+mkdir -p /data/ojalvo/ggHttMC-2-SUBStage2/dags/daginputs
+## outputdir = srm://cmssrm.hep.wisc.edu:8443/srm/v2c2/server?SFN=/hdfs/store/user/ojalvo/ggHttMC-2-SUBStage2/
 #Matching E and H activity
-#farmoutAnalysisJobs  --input-file-list=RAW-Run2015D.txt --no-shared-fs  --submit-dir=/data/ojalvo/ggHttMC-SUBStage2/submit --output-dag-file=/data/ojalvo/ggHttMC-SUBStage2/dags/dag --output-dir=srm://cmssrm.hep.wisc.edu:8443/srm/v2c2/server?SFN=/hdfs/store/user/ojalvo/ggHttMC-SUBStage2/ ZeroBias-260627   $CMSSW_BASE  $CMSSW_BASE/src/L1Trigger/Stage3Ntuplizer/test/SUBStage2.py 
+#farmoutAnalysisJobs  --input-file-list=RAW-Run2015D.txt --no-shared-fs  --submit-dir=/data/ojalvo/ggHttMC-2-SUBStage2/submit --output-dag-file=/data/ojalvo/ggHttMC-2-SUBStage2/dags/dag --output-dir=srm://cmssrm.hep.wisc.edu:8443/srm/v2c2/server?SFN=/hdfs/store/user/ojalvo/ggHttMC-2-SUBStage2/ ZeroBias-260627   $CMSSW_BASE  $CMSSW_BASE/src/L1Trigger/Stage3Ntuplizer/test/SUBStage2.py 
 
 farmoutAnalysisJobs --assume-input-files-exist  --input-file-list=ggHtt-76X-TSG-MINI.txt \
---submit-dir=/data/ojalvo/ggHttMC-SUBStage2/submit \
---output-dag-file=/data/ojalvo/ggHttMC-SUBStage2/dags/dag \
-ggHttMC  \
+--submit-dir=/data/ojalvo/ggHttMC-2-SUBStage2/submit \
+--output-dag-file=/data/ojalvo/ggHttMC-2-SUBStage2/dags/dag \
+ggHttMC-2  \
 $CMSSW_BASE  \
 $CMSSW_BASE/src/L1Trigger/Stage3Ntuplizer/test/SUBStage2.py farmout=True rates=False data=False
 
